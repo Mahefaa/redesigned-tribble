e9dc96866e44c7f045a9c524f46ca0a99e261dff
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2022-05-27 15:47:10 +0300

    update: editing sidebar items seedingé

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,36 +1,58 @@
 import { SidebarMenuHeading } from "./SidebarMenuHeading";
 import { NavLink } from "./NavLink";
 
+const sidebarItems = [
+  { type: "heading", title: "Core" },
+  { type: "navlink", title: "Dashboard", icon: "fas fa-tachometer-alt" },
+  { type: "heading", title: "Interface" },
+  {
+    type: "navlink",
+    title: "Dashboard",
+    icon: "fas fa-columns",
+    items: ["Static navigation", "Light sidenav"],
+  },
+  {
+    type: "navlink",
+    title: "Pages",
+    icon: "fas fa-columns",
+    items: [
+      {
+        type: "navlink",
+        title: "Authentication",
+        items: ["Login", "Register", "Forget password"],
+      },
+      {
+        type: "navlink",
+        title: "Error",
+        items: ["400 error", "4O1 error", "500 error"],
+      },
+    ],
+  },
+  { type: "heading", title: "Addons" },
+  {
+    type: "navlink",
+    title: "Charts",
+    icon: "fas fa-chart-area",
+  },
+  {
+    type: "navlink",
+    title: "Tables",
+    icon: "fas fa-table",
+  },
+];
+
 export function Sidebar() {
   return (
     <nav className="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
       <div className="sb-sidenav-menu">
         <div className="nav">
-          <SidebarMenuHeading title="Core" />
-          <NavLink title="Dashboard" icon="fas fa-tachometer-alt" />
-          <SidebarMenuHeading title="Interface" />
-          <NavLink
-            title="Layouts"
-            icon="fas fa-columns"
-            items={["Static Navigation", "Light Sidenav"]}
-          />
-          <NavLink
-            title="Pages"
-            icon="fas fa-columns"
-            items={[
-              {
-                title: "Authentication",
-                items: ["Login", "Register", "Forget Password"],
-              },
-              {
-                title: "Error",
-                items: ["401 Error", "404 Pages", "500 Pages"],
-              },
-            ]}
-          />
-          <SidebarMenuHeading title="Addons"></SidebarMenuHeading>
-          <NavLink icon="fas fa-chart-area" title="Charts" />
-          <NavLink icon="fas fa-table" title="Tables" />
+          {sidebarItems.map((sideBarItem) =>
+            sideBarItem.type === "heading" ? (
+              <SidebarMenuHeading {...sideBarItem} />
+            ) : (
+              <NavLink {...sideBarItem} />
+            )
+          )}
         </div>
       </div>
       <div className="sb-sidenav-footer">
