89facf371050a079b9e4352f74c1df0a1347dcd0
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2022-05-27 15:29:24 +0300

    feat: splitting sidebar into multiple components

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,150 +1,88 @@
+import { useState } from "react";
+
+export function SidebarMenuHeading(props) {
+  const { title } = props;
+  return <div className="sb-sidenav-menu-heading">{title}</div>;
+}
+
+export function NavLink(props) {
+  const { icon, title, items } = props;
+  const [navLinkClass, setNavLinkClass] = useState("nav-link collapsed");
+  const [itemsClass, setItemsClass] = useState("collapse");
+
+  function toggleNavLink() {
+    setNavLinkClass(
+      navLinkClass.includes("collapsed") ? "nav-link" : "nav-link collapsed"
+    );
+    setItemsClass(itemsClass.includes("show") ? "collapse" : "collapse show");
+  }
+
+  return (
+    <>
+      <a className={navLinkClass} onClick={toggleNavLink}>
+        {icon && (
+          <div className="sb-nav-link-icon">
+            <i className={icon}></i>
+          </div>
+        )}
+        {title}
+        {items && items.length && (
+          <div className="sb-sidenav-collapse-arrow">
+            <i className="fas fa-angle-down"></i>
+          </div>
+        )}
+      </a>
+      {items && items.length && (
+        <div className={itemsClass}>
+          <nav className="sb-sidenav-menu-nested nav">
+            {items.map((item) =>
+              typeof item === "string" ? (
+                <a className="nav-link">{item}</a>
+              ) : (
+                <NavLink
+                  icon={item.icon}
+                  title={item.title}
+                  items={item.items}
+                />
+              )
+            )}
+          </nav>
+        </div>
+      )}
+    </>
+  );
+}
+
 export function Sidebar() {
   return (
     <nav className="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
       <div className="sb-sidenav-menu">
         <div className="nav">
-          <div className="sb-sidenav-menu-heading">Core</div>
-          <a className="nav-link" href="index.html">
-            <div className="sb-nav-link-icon">
-              <i className="fas fa-tachometer-alt"></i>
-            </div>
-            Dashboard
-          </a>
-          <div className="sb-sidenav-menu-heading">Interface</div>
-          <a
-            className="nav-link collapsed"
-            href="src/components/Sidebar#"
-            data-bs-toggle="collapse"
-            data-bs-target="#collapseLayouts"
-            aria-expanded="false"
-            aria-controls="collapseLayouts"
-          >
-            <div className="sb-nav-link-icon">
-              <i className="fas fa-columns"></i>
-            </div>
-            Layouts
-            <div className="sb-sidenav-collapse-arrow">
-              <i className="fas fa-angle-down"></i>
-            </div>
-          </a>
-          <div
-            className="collapse"
-            id="collapseLayouts"
-            aria-labelledby="headingOne"
-            data-bs-parent="#sidenavAccordion"
-          >
-            <nav className="sb-sidenav-menu-nested nav">
-              <a className="nav-link" href="layout-static.html">
-                Static Navigation
-              </a>
-              <a className="nav-link" href="layout-sidenav-light.html">
-                Light Sidenav
-              </a>
-            </nav>
-          </div>
-          <a
-            className="nav-link collapsed"
-            href="src/components/Sidebar#"
-            data-bs-toggle="collapse"
-            data-bs-target="#collapsePages"
-            aria-expanded="false"
-            aria-controls="collapsePages"
-          >
-            <div className="sb-nav-link-icon">
-              <i className="fas fa-book-open"></i>
-            </div>
-            Pages
-            <div className="sb-sidenav-collapse-arrow">
-              <i className="fas fa-angle-down"></i>
-            </div>
-          </a>
-          <div
-            className="collapse"
-            id="collapsePages"
-            aria-labelledby="headingTwo"
-            data-bs-parent="#sidenavAccordion"
-          >
-            <nav
-              className="sb-sidenav-menu-nested nav accordion"
-              id="sidenavAccordionPages"
-            >
-              <a
-                className="nav-link collapsed"
-                href="src/components/Sidebar#"
-                data-bs-toggle="collapse"
-                data-bs-target="#pagesCollapseAuth"
-                aria-expanded="false"
-                aria-controls="pagesCollapseAuth"
-              >
-                Authentication
-                <div className="sb-sidenav-collapse-arrow">
-                  <i className="fas fa-angle-down"></i>
-                </div>
-              </a>
-              <div
-                className="collapse"
-                id="pagesCollapseAuth"
-                aria-labelledby="headingOne"
-                data-bs-parent="#sidenavAccordionPages"
-              >
-                <nav className="sb-sidenav-menu-nested nav">
-                  <a className="nav-link" href="login.html">
-                    Login
-                  </a>
-                  <a className="nav-link" href="register.html">
-                    Register
-                  </a>
-                  <a className="nav-link" href="password.html">
-                    Forgot Password
-                  </a>
-                </nav>
-              </div>
-              <a
-                className="nav-link collapsed"
-                href="src/components/Sidebar#"
-                data-bs-toggle="collapse"
-                data-bs-target="#pagesCollapseError"
-                aria-expanded="false"
-                aria-controls="pagesCollapseError"
-              >
-                Error
-                <div className="sb-sidenav-collapse-arrow">
-                  <i className="fas fa-angle-down"></i>
-                </div>
-              </a>
-              <div
-                className="collapse"
-                id="pagesCollapseError"
-                aria-labelledby="headingOne"
-                data-bs-parent="#sidenavAccordionPages"
-              >
-                <nav className="sb-sidenav-menu-nested nav">
-                  <a className="nav-link" href="401.html">
-                    401 Page
-                  </a>
-                  <a className="nav-link" href="404.html">
-                    404 Page
-                  </a>
-                  <a className="nav-link" href="500.html">
-                    500 Page
-                  </a>
-                </nav>
-              </div>
-            </nav>
-          </div>
-          <div className="sb-sidenav-menu-heading">Addons</div>
-          <a className="nav-link" href="charts.html">
-            <div className="sb-nav-link-icon">
-              <i className="fas fa-chart-area"></i>
-            </div>
-            Charts
-          </a>
-          <a className="nav-link" href="tables.html">
-            <div className="sb-nav-link-icon">
-              <i className="fas fa-table"></i>
-            </div>
-            Tables
-          </a>
+          <SidebarMenuHeading title="Core" />
+          <NavLink title="Dashboard" icon="fas fa-tachometer-alt" />
+          <SidebarMenuHeading title="Interface" />
+          <NavLink
+            title="Layouts"
+            icon="fas fa-columns"
+            items={["Static Navigation", "Light Sidenav"]}
+          />
+          <NavLink
+            title="Pages"
+            icon="fas fa-columns"
+            items={[
+              {
+                title: "Authentication",
+                items: ["Login", "Register", "Forget Password"],
+              },
+              {
+                title: "Error",
+                items: ["401 Error", "404 Pages", "500 Pages"],
+              },
+            ]}
+          />
+          <SidebarMenuHeading title="Addons"></SidebarMenuHeading>
+          <NavLink icon="fas fa-chart-area" title="Charts" />
+          <NavLink icon="fas fa-table" title="Tables" />
         </div>
       </div>
       <div className="sb-sidenav-footer">
