f81b0761d4a7070d281113df1c60d72151a479c1
Mahefa Ny Anjara <hei.mahefa@gmail.com>
2022-06-28 14:12:12 +0300

    feat : add Modal

--- src/App.js
+++ src/App.js
@@ -7,20 +7,17 @@ import { Footer } from "./components/Footer";
 import { Card } from "./components/Card";
 import {useEffect, useState} from "react";
 import axios from "axios";
-
 function App() {
   const [employees,setEmployeesList] = useState([]);
-
   const [sidebarClass, setSidebarClass] = useState("sb-nav-fixed");
-
+  const [showModal,setShowModal] = useState(false)
   useEffect(() => {
     axios.get("https://jsonplaceholder.typicode.com/users").then((response) => {
       const {data} = response;
       setEmployeesList(data);
-      console.log(data);
     }).catch((error) => console.error(error));
   },[])
-
+  //setEmployeesList([{id:"1"}]);
   function toggleSidebarClass() {
     setSidebarClass(
       sidebarClass.includes("toggled")
@@ -28,30 +25,23 @@ function App() {
         : "sb-nav-fixed sb-sidenav-toggled"
     );
   }
-
   return (
     <div className={sidebarClass}>
       <Navbar toggleSidebarClass={toggleSidebarClass} />
       <div id="layoutSidenav">
         <div id="layoutSidenav_nav">
-          <Sidebar />
+          <Sidebar/>
         </div>
         <div id="layoutSidenav_content">
           <main>
             <div className="container-fluid px-4">
               <h1 className="mt-4">Tables</h1>
-              <Breadcrumb />
+              <Breadcrumb/>
               <Card>
-                DataTables is a third party plugin that is used to generate the
-                demo table below. For more information about DataTables, please
-                visit the 
-                <a target="_blank" href="https://datatables.net/" rel="noreferrer">
-                   official DataTables documentation
-                </a>
-                .
+                This table has been generated using Reactjs.
               </Card>
               <Card title="DataTable Example">
-                <EmployeeList items={employees} />
+                <EmployeeList items={employees} showModal={showModal} setShowModal={setShowModal}/>
               </Card>
             </div>
           </main>
