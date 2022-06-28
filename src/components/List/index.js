fe76d6a0082719195cfc45e1be150221f2ffe97e
Mahefa Ny Anjara <hei.mahefa@gmail.com>
2022-06-28 14:12:43 +0300

    feat : configure List with Modal

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,54 +1,78 @@
 import "./style.css";
+import {useState} from "react";
+import Modal from "../Modal/Modal";
 
 export function EmployeeList(props) {
-  const { items } = props;
+  const { items,showModal,setShowModal } = props;
+  const [data,setData]=useState({});
+  const [method,setMethod] = useState("post");
   return (
-    <div className="dataTable-wrapper dataTable-loading no-footer sortable searchable fixed-columns">
-      <div className="dataTable-top position-relative">
-        <button type="button" className="btn btn-primary position-absolute top-0 end-0 ">Add</button>
-      </div>
-      <div className="dataTable-container">
-        <table className="table-bordered">
-          <thead>
-            <tr>
-              <th>Id</th>
-              <th>Name</th>
-              <th>Username</th>
-              <th>Email</th>
-              <th>Address</th>
-              <th>Phone</th>
-              <th>Website</th>
-              <th>Company</th>
-            </tr>
-          </thead>
-          <tfoot>
-            <tr>
-              <th>Id</th>
-              <th>Name</th>
-              <th>Username</th>
-              <th>Email</th>
-              <th>Address</th>
-              <th>Phone</th>
-              <th>Website</th>
-              <th>Company</th>
-            </tr>
-          </tfoot>
-          <tbody>
-            {(items || []).map((item) => (
-              <tr key={`${item.id}-${item.name}`}>
-                <td>{item.id}</td>
-                <td>{item.name}</td>
-                <td>{item.username}</td>
-                <td>{item.email}</td>
-                <td>{item.address.city}</td>
-                <td>{item.phone}</td>
-                <td>{item.website}</td>
-                <td>{item.company.name}</td>
+      <>
+      <div className="dataTable-wrapper dataTable-loading no-footer sortable searchable fixed-columns">
+        <div className="dataTable-top">
+          <button type="button" className="btn btn-primary" onClick={()=> {
+            setMethod("post")
+            let toFill = {};
+            for (let key in items[0]) {
+              toFill[key] = key.toString();
+            }
+            setData(toFill);
+            setShowModal(true);
+          }}>Add</button>
+        </div>
+        <div className="dataTable-container" onClick={()=>showModal?setShowModal(false):null}>
+          <table className="table-bordered">
+            <thead>
+              <tr>
+                <th>Id</th>
+                <th>Name</th>
+                <th>Username</th>
+                <th>Email</th>
+                <th>Address</th>
+                <th>Phone</th>
+                <th>Website</th>
+                <th>Company</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tfoot>
+              <tr>
+                <th>Id</th>
+                <th>Name</th>
+                <th>Username</th>
+                <th>Email</th>
+                <th>Address</th>
+                <th>Phone</th>
+                <th>Website</th>
+                <th>Company</th>
+              </tr>
+            </tfoot>
+            <tbody>
+              {(items || []).map((item,i) => (
+                <tr key={i} onClick={()=>{
+                  setData(item);
+                  setMethod("put");
+                  setShowModal(true);
+
+                }}>
+                  <td>{item.id}</td>
+                  <td>{item.name}</td>
+                  <td>{item.username}</td>
+                  <td>{item.email}</td>
+                  <td>{item.address.city}</td>
+                  <td>{item.phone}</td>
+                  <td>{item.website}</td>
+                  <td>{item.company.name}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
-    </div>
+    {showModal?
+        <Modal data={data} method={method} setShowModal={setShowModal}/>
+        :
+        null
+    }
+  </>
   );
 }
